8657e98eb2df6c3ae754d42e2447d723ed1dde19
vishwanath546 <54881149+vishwanath546@users.noreply.github.com>
2021-03-30 19:46:04 +0530

    changes done

---
+++ application/hooks/Myclass.php
@@ -0,0 +1,26 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Myclass extends CI_controller {
+
+
+    public function Myfunction(){
+//$CI=& get_instance();
+//
+//$CI->load->library('Session');
+//        $CI->load->helper('url');
+//var_dump($CI->session->user_session);
+//
+//if($CI->session->user_session==null){
+////    $data = array(
+////        'title' => "Login"
+////    );
+////    $this->load->view('dist/auth-login', $data);
+//
+//
+//} else {
+//
+//}
+//
+    }
+
+}

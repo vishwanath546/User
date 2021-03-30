944a068821bd26f7d973916ccd3a983f1603bb7c
vishwanath546 <54881149+vishwanath546@users.noreply.github.com>
2021-03-30 19:54:41 +0530

    changes done

--- application/views/dist/_partials/layout.php
+++ application/views/dist/_partials/layout.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
             <img alt="image" src="<?php echo base_url(); ?>assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
             <div class="d-sm-none d-lg-inline-block"><?php if(!is_null($this->session->user_session)){
-                echo $this->session->user_session->first_name;
+//                echo $this->session->user_session->first_name;
             }
             ?></div></a>
             <div class="dropdown-menu dropdown-menu-right">
